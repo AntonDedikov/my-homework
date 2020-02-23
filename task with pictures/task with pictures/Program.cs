3d666e6c1e23c342400304c2beddc0b50250cbe9
AntonDedikov <61086734+AntonDedikov@users.noreply.github.com>
2020-02-23 12:33:35 +0200

    task with pictures

---
+++ task with pictures/task with pictures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_with_pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userPictures = 52;
+            int seriesPictures = userPictures / 3;
+            int lostPictures = userPictures % 3;
+
+            Console.WriteLine("Количество заполненных рядов: " + seriesPictures);
+            Console.WriteLine("Сверх меры: " + lostPictures);
+            Console.ReadKey();
+        }
+    }
+}
