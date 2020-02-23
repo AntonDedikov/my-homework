6c617fba9469e278f4d412762acb4a15f0e75f1c
AntonDedikov <61086734+AntonDedikov@users.noreply.github.com>
2020-02-23 13:38:16 +0200

    task for shop

---
+++ task for shop/task for shop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_for_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int clientCash;
+            int crystalCost = 15;
+            int countCristals;
+            bool control;
+
+            Console.Write("Ваши средства: ");
+            clientCash = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов вы хотите приобрести?: ");
+            countCristals = Convert.ToInt32(Console.ReadLine());
+
+
+            control = clientCash >= crystalCost * countCristals;
+            countCristals *= Convert.ToInt32(control);
+
+            clientCash -= crystalCost * countCristals;
+
+            Console.WriteLine("вы приобрели: " + countCristals + " кристаллов");
+            Console.WriteLine("у вас осталось: " + clientCash + " золота");
+            Console.ReadKey();
+
+        }
+    }
+}
