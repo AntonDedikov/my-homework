bbfb6d1323c2e92df3c58bab5031de6cacd361f9
AntonDedikov <61086734+AntonDedikov@users.noreply.github.com>
2020-02-26 12:42:14 +0200

    Program with password

---
+++ Program with password/Program with password/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_with_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string passwordForMessage = "1234";
+            string userInput;
+            int attempts = 3;
+
+            Console.WriteLine("Введите пароль:");
+            userInput = Console.ReadLine();
+            for(attempts > 0; attemps--)
+            {
+                if (userInput == passwordForMessage)
+                {
+                    Console.WriteLine("42");
+                }
+                else
+                {
+                    Console.WriteLine("Пароль не верный. Попробуйте еще раз."
+                        + "У вас осталось " + attempts + " попыток.");
+                }
+            }
+        }
+    }
+}
---
+++ Sequence/Sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 98; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
