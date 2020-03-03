14b0786f143da50d7844efa8ebb345fb257d434a
AntonDedikov <61086734+AntonDedikov@users.noreply.github.com>
2020-03-03 15:24:04 +0200

    Work with specific rows columns

---
+++ Console program/Console program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SetName - что бы ввести имя");
+            Console.WriteLine("ChangeConsoleColor - что бы изменить цвет консоли");
+            Console.WriteLine("SetNameColor - установить цвет имени");
+            Console.WriteLine("ChangeName - изменить имя");
+            Console.WriteLine("SetPassword - установить пароль");
+            Console.WriteLine("Change Password - изменить пароль");
+            Console.WriteLine("");
+            Console.WriteLine("Esc - выйти из программы");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Work with specific rows columns/Work with specific rows columns/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_specific_rows_columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] number = new int[3, 5] { { 10, 2, 3, 4, 5 }, { 20, 7, 4, 4, 5 }, { 10, 2, 3, 4, 5 } };
+            int sumTwoString = 0;
+            int firstRowProduct = 1;
+
+            for (int i = 0; i < number.GetLength(0); i++)
+            {
+                for (int j = 0; j < number.GetLength(1); j++)
+                {
+                    if (i == 1)
+                    {
+                        sumTwoString += number[i, j];
+                    }
+                    if (j == 0)
+                    {
+                        firstRowProduct *= number[i, j];
+                    }
+                    Console.Write(number[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Сумма второй строки " + sumTwoString);
+            Console.WriteLine("Произведение первого столбца " + firstRowProduct);
+            Console.ReadKey();
+        }
+    }
+}
