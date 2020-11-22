832a85b0283fc5ce6972e86c8c2457e45c3fbbd8
AntonDedikov <dedikov.99@gmail.com>
2020-11-22 11:52:09 +0300

    consoleProgram

--- Console program/Console program/Program.cs
+++ Console program/Console program/Program.cs
@@ -10,15 +10,122 @@ namespace Console_program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SetName - что бы ввести имя");
-            Console.WriteLine("ChangeConsoleColor - что бы изменить цвет консоли");
-            Console.WriteLine("SetNameColor - установить цвет имени");
-            Console.WriteLine("ChangeName - изменить имя");
-            Console.WriteLine("SetPassword - установить пароль");
-            Console.WriteLine("Change Password - изменить пароль");
-            Console.WriteLine("");
-            Console.WriteLine("Esc - выйти из программы");
-            Console.ReadKey();
+            bool exit = false;
+            string name = "name";
+            int password = 0;
+            while (exit == false) 
+            {
+                if (name == "name")
+                {
+                    Console.WriteLine("SetName - что бы ввести имя");
+                }
+                else
+                {
+                    Console.WriteLine("имя установлено");
+                }
+                Console.WriteLine("ChangeConsoleColor - что бы изменить цвет консоли");
+                Console.WriteLine("ChangeTextColor - установить цвет текста");
+                if (password == 0)
+                {
+                    Console.WriteLine("SetPassword - установить пароль");
+                }
+                else
+                {
+                    Console.WriteLine("пароль установлен");
+                }
+                Console.WriteLine("WriteName - вывести имя");
+                Console.WriteLine("Esc - выйти из программы");
+
+                switch (Console.ReadLine())
+                {
+                    case "SetName":
+                        Console.WriteLine("Введите имя:");
+                        name = Console.ReadLine();
+                        break;
+                    case "ChangeConsoleColor":
+                        Console.WriteLine("Выберете цвет:");
+                        Console.WriteLine("black, green, blue, red, yellow, white");
+                        switch (Console.ReadLine())
+                        {
+                            case "black":
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                break;
+                            case "green":
+                                Console.BackgroundColor = ConsoleColor.Green;
+                                break;
+                            case "blue":
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                                break;
+                            case "red":
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                break;
+                            case "yellow":
+                                Console.BackgroundColor = ConsoleColor.Yellow;
+                                break;
+                            case "white":
+                                Console.BackgroundColor = ConsoleColor.White;
+                                break;
+                        }
+                        break;
+                    case "ChangeTextColor":
+                        Console.WriteLine("Выберете цвет:");
+                        Console.WriteLine("black, green, blue, red, yellow, white");
+                        switch (Console.ReadLine())
+                        {
+                            case "black":
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                break;
+                            case "green":
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                break;
+                            case "blue":
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                break;
+                            case "red":
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                break;
+                            case "yellow":
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                break;
+                            case "white":
+                                Console.ForegroundColor = ConsoleColor.White;
+                                break;
+                        }
+                        break;
+                    case "SetPassword":
+                        Console.WriteLine("Введите желаемый пароль:");
+                        password = Convert.ToInt32(Console.ReadLine());
+                        break; 
+                    case "WriteName":
+                        int userInput;
+                        int inputAttempts = 3;
+                        if (password != 0)
+                        {
+                            while (inputAttempts-- > 0)
+                            {
+                                Console.WriteLine("Для ввывода имени введите пароль:");
+                                userInput = Convert.ToInt32(Console.ReadLine());
+                                if (userInput == password)
+                                {
+                                    Console.WriteLine(name);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("У вас осталось " + inputAttempts + " попыток.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(name);
+                        }
+                        break;
+                    case "Esc":
+                        exit = true;
+                        break;
+                }
+                Console.Clear();
+            }
         }
     }
 }
