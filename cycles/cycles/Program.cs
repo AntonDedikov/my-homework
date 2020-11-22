5cc3058309b120177941a0a0f36ae87f1c2efef3
AntonDedikov <dedikov.99@gmail.com>
2020-11-22 08:47:06 +0300

    cycles

---
+++ cycles/cycles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int repeatNumber;
+
+            Console.WriteLine("Впишите сообщение:");
+            userInput = Console.ReadLine();
+            Console.WriteLine("Сколько раз повторить:");
+            repeatNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repeatNumber; i++)
+            {
+                Console.WriteLine(userInput);
+            }
+            Console.ReadKey();
+        }
+    }
+}
