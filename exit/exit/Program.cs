3b8963db8e62b0ea5fe14a018dd1aabd99c180c3
AntonDedikov <dedikov.99@gmail.com>
2020-11-23 10:09:32 +0300

    exit

---
+++ exit/exit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (exit == false)
+            {
+                Console.WriteLine("Введите exit для выключения программы:");
+                string userInput = Console.ReadLine();
+                if(userInput == "exit")
+                {
+                    exit = true;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
