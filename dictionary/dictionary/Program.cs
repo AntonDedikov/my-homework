c07822448b2a32d7d657a3fb8d4daf04e14011f9
AntonDedikov <dedikov.99@gmail.com>
2020-11-26 13:00:43 +0300

    dictionary

--- dictionary/dictionary/Program.cs
+++ dictionary/dictionary/Program.cs
@@ -14,26 +14,26 @@ namespace dictionary
             {
                 Dictionary<string, string> words = new Dictionary<string, string>();
                 words.Add("ахэгао", "ахэгао - это favicon на главной странице ЯЮниор");
-                words.Add("нэко", "нэко - это кошка или девушка, похожая на неё");
+                words.Add("сиеста", "сиеста - послеобеденный отдых");
                 words.Add("катарсис", "катарсис - это нравственное очищение в результате \n душевного потрясения или перенесённого страдания");
                 words.Add("рефлексия", "рефлексия - это размышление о своём психологическом \n состоянии, склонность анализировать свои переживания");
-                Console.WriteLine("Великий словарь содержит такие слова: ахэгао, нэко, катарсис, рефлексия");
+                Console.WriteLine("Великий словарь содержит такие слова: ахэгао, сиеста, катарсис, рефлексия");
                 Console.Write("Введите слово:");
                 string userInput = Console.ReadLine();
 
-                int i = 0;
-                foreach (var word in words)
+                if (words.ContainsKey(userInput))
                 {
-                    i++;
-                    if (word.Key.ToLower() == userInput.ToLower())
+                    foreach (var word in words)
                     {
-                        Console.WriteLine(word.Value);
-                        i = 5;
+                        if (word.Key == userInput)
+                        {
+                            Console.WriteLine(word.Value);
+                        }
                     }
-                    if (words.Count == i)
-                    {
-                        Console.WriteLine("Вы вероятно ошиблись");
-                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы вероятно ошиблись");
                 }
                 Console.ReadKey();
                 Console.Clear();
