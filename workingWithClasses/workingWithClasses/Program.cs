341504c1e06508253ad3368984babd71048b6083
AntonDedikov <dedikov.99@gmail.com>
2020-12-02 11:17:37 +0300

    workingWithClass

---
+++ workingWithClasses/workingWithClasses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingWithClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Введите имя:");
+            string name = Console.ReadLine(); 
+
+            Player player1 = new Player(name);
+
+            player1.ShowInfo();
+            Console.ReadKey();
+        }
+        class Player
+        {
+            string Name;
+            int Health;
+            int Spead;
+            int Damage;
+            public Player(string name)
+            {
+                Name = name;
+                Health = 100;
+                Spead = 5;
+                Damage = 10;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($" Имя игрока = {Name}\n Жизни = {Health}\n Скорость = {Spead}\n Урон = {Damage}\n");
+            }
+        }
+    }
+}
