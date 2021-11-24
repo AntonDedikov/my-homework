075a4424e6f436ffc65f3822762cb64155a096a7
AntonDedikov <dedikov.99@gmail.com>
2021-11-24 22:19:48 +0200

    player database

---
+++ player database/player database/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOpen = true;
+
+            DataBase dataBase = new DataBase();
+
+            while (isOpen)
+            {
+                Console.SetCursorPosition(20, 1);
+                Console.WriteLine("База данных");
+                dataBase.ShowPlayers();
+
+                Console.SetCursorPosition(3, 16);
+                Console.WriteLine("Доступные команды:\n" +
+                    "1 - Добавить игрока\n" +
+                    "2 - Забанить\n" +
+                    "3 - Разбанить\n" +
+                    "4 - Удалить");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.NumPad1)
+                {
+                    dataBase.AddPlayer();
+                }
+                else
+                {
+                    dataBase.GetPlayer(key);
+                }
+                Console.Clear();
+            }
+        }
+    }
+
+    class DataBase
+    {
+        private List<Player> players = new List<Player>();
+
+        public void AddPlayer()
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Введите ник игрока:");
+            string nickName = Console.ReadLine();
+            int level = random.Next(0, 81);
+            bool ban = false;
+
+            players.Add(new Player(nickName, level, ban));
+        }
+
+        public void GetPlayer(ConsoleKeyInfo key)
+        {
+            Console.WriteLine("Введите порядковый номер игрока:");
+            int PlayerNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (key.Key)
+            {
+                case ConsoleKey.NumPad2:
+                    players[PlayerNumber].BanPlayer();
+                    break;
+                case ConsoleKey.NumPad3:
+                    players[PlayerNumber].UnBanPlayer();
+                    break;
+                case ConsoleKey.NumPad4:
+                    players.RemoveAt(PlayerNumber);
+                    break;
+            }
+        }
+
+        public void ShowPlayers()
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                Console.Write(i);
+                players[i].ShowInfo();
+            }
+        }
+    }
+
+    class Player
+    {
+        private string _nickname;
+        private int _level;
+        private bool _ban;
+
+        public Player(string nickName, int level, bool ban)
+        {
+            _nickname = nickName;
+            _level = level;
+            _ban = ban;
+        }
+
+        public void BanPlayer()
+        {
+            _ban = true;
+        }
+
+        public void UnBanPlayer()
+        {
+            _ban = false;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($" Игрок - {_nickname} уровень - {_level} бан - {_ban}");
+        }
+    }
+}
---
+++ player database/player database/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("player database")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("player database")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1a9071a7-485d-40c7-814a-669851ac84c9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
